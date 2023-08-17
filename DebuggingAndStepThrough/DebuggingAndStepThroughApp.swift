2bed3d3fdfb15dc1d42e584a3b783be3998537f5
Pac12Run8000 <gangstarr78992@>
2023-08-17 05:15:15 -0700

    Initial Commit

---
+++ DebuggingAndStepThrough/DebuggingAndStepThroughApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebuggingAndStepThroughApp.swift
+//  DebuggingAndStepThrough
+//
+//  Created by Michelle Grover on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct DebuggingAndStepThroughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
