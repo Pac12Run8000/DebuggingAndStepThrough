89dad6f1d878ef2149dd6b0f2230076db69a38f8
Pac12Run8000 <gangstarr78992@>
2023-08-17 05:40:15 -0700

    Getting back Json data

--- DebuggingAndStepThrough/ContentView.swift
+++ DebuggingAndStepThrough/ContentView.swift
@@ -6,17 +6,40 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Button(action: {
+                    Task {
+                        await fetchData()
+                    }
+                }) {
+                    Text("Fetch Data")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
         }
-        .padding()
-    }
+        
+        func fetchData() async {
+            guard let url = URL(string: "https://datausa.io/api/data?drilldowns=Nation&measures=Population&year=latest") else {
+                print("Invalid URL")
+                return
+            }
+            
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                if let jsonString = String(data: data, encoding: .utf8) {
+                    print(jsonString)
+                }
+            } catch {
+                print("HTTP Request Failed \(error)")
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
